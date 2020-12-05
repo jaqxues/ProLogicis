33d8e3b9e41d3139ec63a04326e977967de09b56
jaqxues <32979131+jaqxues@users.noreply.github.com>
2020-12-05 10:46:57 +0100

    Add Sentence.kt

---
+++ core/src/commonMain/kotlin/com/jaqxues/prologicis/Sentence.kt
@@ -0,0 +1,52 @@
+package com.jaqxues.prologicis
+
+
+/**
+ * This file was created by Jacques Hoffmann (jaqxues) in the Project ProLogicis.<br>
+ * Date: 05.12.20 - Time 10:42.
+ */
+typealias Biconditional = Equality
+
+abstract class Sentence {
+    abstract fun format(): String
+
+    infix fun implies(other: Sentence) = Implication(this, other)
+    infix fun or(other: Sentence) = Or(this, other)
+    infix fun xor(other: Sentence) = Xor(this, other)
+    infix fun and(other: Sentence) = And(this, other)
+    infix fun iif(other: Sentence) = Biconditional(this, other)
+}
+
+fun not(sentence: Sentence) = Not(sentence)
+
+data class Symbol(val proposition: String): Sentence() {
+    override fun format() = proposition
+}
+
+data class Or(val sentences: List<Sentence>): Sentence() {
+    constructor(vararg sentences: Sentence): this(sentences.toList())
+
+    override fun format() = sentences.joinToString(separator = " or ") { it.format() }
+}
+
+data class Xor(val sentences: List<Sentence>): Sentence() {
+    constructor(vararg sentences: Sentence): this(sentences.toList())
+
+    override fun format() = sentences.joinToString(separator = " xor ") { it.format() }
+}
+
+data class And(val sentences: List<Sentence>): Sentence() {
+    constructor(vararg sentences: Sentence): this(sentences.toList())
+
+    override fun format() = sentences.joinToString(separator = " and ") { it.format() }
+}
+
+data class Not(val sentence: Sentence): Sentence() {
+    override fun format() = "Not(${sentence.format()})"
+}
+data class Implication(val s1: Sentence, val s2: Sentence): Sentence() {
+    override fun format() = "${s1.format()} -> ${s2.format()}"
+}
+data class Equality(val s1: Sentence, val s2: Sentence): Sentence() {
+    override fun format() = "${s1.format()} <-> ${s2.format()}"
+}
